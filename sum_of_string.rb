a3ee201b8f8b616a0197a2159ac13b8897d47fdf
Alen George <al3ngeorge@gmail.com>
2024-06-30 17:24:50 +0530

    format code

--- sum_of_string.rb
+++ sum_of_string.rb
@@ -1,9 +1,7 @@
 def add(string)
     seperator = ","
     if string.start_with?("//")
-      newline_index = string.index("\n")
-      seperator = string[string.index("//")+2...newline_index]
-      string = string[newline_index+1..]
+      seperator = f_delimiter_and_truncate(string)
     end
 
     if string.include?("\n")
@@ -13,13 +11,7 @@ def add(string)
     if(string.length > 1)
       arr_format = string.split('')
       if arr_format.include?('-') and seperator != '-'
-        negative_numbers = []
-        arr_format.each_with_index do |char,index|
-          if char == '-' and integer_string?(arr_format[index+1])
-            negative_numbers << char+arr_format[index+1]
-          end
-        end
-        raise ArgumentError, "negative numbers are not allowed " + negative_numbers.join(',') if negative_numbers.any?
+        handle_negative_numbers(arr_format)
       elsif arr_format.include?(seperator)
         return arr_format.join('').split(seperator).map(&:to_i).sum
       else
@@ -29,6 +21,24 @@ def add(string)
     string.to_i
   end
 
+
+  def f_delimiter_and_truncate(string)
+      newline_index = string.index("\n")
+      seperator = string[string.index("//")+2...newline_index]
+      string = string[newline_index+1..]
+      seperator
+  end
+
+
+  def handle_negative_numbers(arr_format)
+    negative_numbers = []
+    arr_format.each_with_index do |char,index|
+      if char == '-' and integer_string?(arr_format[index+1])
+        negative_numbers << char+arr_format[index+1]
+      end
+    end
+    raise ArgumentError, "negative numbers are not allowed " + negative_numbers.join(',') if negative_numbers.any?
+  end
 
   def integer_string?(str)
     return false if str.nil?
