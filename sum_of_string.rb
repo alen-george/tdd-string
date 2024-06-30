84ddb81b1d9dbe76491d7c907d3c3846708dbc01
Alen George <al3ngeorge@gmail.com>
2024-06-30 14:35:10 +0530

    CSV integer input provided

--- sum_of_string.rb
+++ sum_of_string.rb
@@ -2,7 +2,11 @@ def add(string)
     return 0 unless string.length
 
     if(string.length > 1)
-      return string.split('').map(&:to_i).sum
+      arr_format = string.split('')
+      if arr_format.include?(',')
+        return arr_format.join('').split(',').map(&:to_i).sum
+      end
+      return arr_format.map(&:to_i).sum
     end
     string.to_i
   end
--- sum_of_string_spec.rb
+++ sum_of_string_spec.rb
@@ -17,12 +17,17 @@ describe 'Sum of integers from the String' do
     end
 
     context "a double digit integer in string format" do
-      it 'return the digit' do
-        expect(add('81')).to eq 9
+      it 'return the sum of digits' do
+        expect(add('813')).to eq 12
       end
 
     end
 
+    context "a CSV of integers" do
+      it 'return the sum of all digits' do
+        expect(add('8,1,3')).to eq 12
+      end
+    end
   end
 
 end
